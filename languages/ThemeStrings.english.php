ac45bac95e675a9592b1994838b7bdfba88dd7d0
Diego Andrés <diegoandres_cortes@outlook.com>
2021-11-06 17:31:47 -0600

    release for gh
    Signed-off-by: Diego Andrés <diegoandres_cortes@outlook.com>

---
+++ languages/ThemeStrings.english.php
@@ -0,0 +1,17 @@
+<?php
+
+$txt['lgf_desc_own'] = 'Add your own description';
+$txt['lgf_desc_own_desc'] = 'Here you can add your own description in the footer of the forum';
+$txt['lgf_desc_own_default'] = 'Add your own description in your admin center';
+$txt['facebook_url'] = 'Facebook URL';
+$txt['twitter_url'] = 'Twitter URL';
+$txt['youtube_url'] = 'Youtube URL';
+$txt['rss_url'] = 'RSS URL';
+
+$txt['costa_topico_fixo'] = 'Sticky Topics';
+$txt['costa_topico_normal'] = 'Normal Topics';
+
+$txt['separate_sticky_locked'] = 'Separate sticky topics';
+$txt['separate_sticky_locked_desc'] = 'With this, you can separate the sticky topics and your normal topics';
+
+?>
